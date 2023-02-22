644bf05c122760b0269930beda8e09b1d989e919
AdrianDiaz <cesarqqu@gmail.com>
2023-02-21 21:05:42 -0600

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express"
+const router = Router()
+
+router.get('/', (req, res) => {
+    return res.render('index', {title:'HomePage'})
+})
+router.get('/about', (req, res) => {
+    return res.render('about', { title: 'About me' })
+})
+router.get('/contact', (req, res) => {
+    return res.render('contact',{ title: 'Contact' })
+})
+
+export default router
