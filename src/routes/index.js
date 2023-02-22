8fba239c8060a2b7d5d58e6aa427a52cd998b858
AdrianDiaz <cesarqqu@gmail.com>
2023-02-21 21:35:17 -0600

    deployed

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 const router = Router()
 
 router.get('/', (req, res) => {
-    return res.render('index', {title:'HomePage'})
+    return res.render('index', {title:'Adrian'})
 })
 router.get('/about', (req, res) => {
     return res.render('about', { title: 'About me' })
