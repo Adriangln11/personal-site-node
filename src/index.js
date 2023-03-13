36b15183aada9f0f1aa523eeca014ae448a4be5c
AdrianDiaz <cesarqqu@gmail.com>
2023-03-12 21:06:30 -0600

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import express  from 'express'
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
-import indexRouter from './routes/index.js'
+require('dotenv').config()
+const express = require('express') 
+const path = require('path')
+const indexRouter = require('./routes/index.js') 
 
 const app = express()
+app.set('port', process.env.PORT || 3000)
+app.use(express.urlencoded({ extended:false }))
+app.use(express.json())
 app.set('view engine', 'ejs')
-const __dirname = dirname(fileURLToPath(import.meta.url))
-app.set('views', join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(indexRouter)
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost${PORT}`)
+app.listen(app.get('port'), () => {
+    console.log(`Server listening on http://localhost${app.get('port')}`)
 })
