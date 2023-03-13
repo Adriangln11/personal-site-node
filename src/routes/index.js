a5eb93eab8a684b0cb6e6f68b88c3d19399e2af9
AdrianDiaz <cesarqqu@gmail.com>
2023-03-12 21:07:53 -0600

    route form request

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
-import { Router } from "express"
-const router = Router()
+const  express = require('express')
+const router = express.Router()
+const transporter = require('../config/transporter.js')
 
 router.get('/', (req, res) => {
     return res.render('index', {title:'Adrian'})
@@ -10,5 +11,20 @@ router.get('/about', (req, res) => {
 router.get('/contact', (req, res) => {
     return res.render('contact',{ title: 'Contact' })
 })
+router.post('/form-request', async (req, res) => {
+    const {email, message} = req.body
+    console.log(email, message)
+    await transporter.sendMail({
+        from: email,
+        to: process.env.EMAIL,
+        subject: 'Contact from your site',
+        html: `
+            <p>${message}</p>
+            </br>
+            <b>Message sent by ${email}</b>
+        `
+    })
+    return res.render('index', {title: 'Adrian'})
+})
 
-export default router
+module.exports = router
